5b601bd6928533c69d91a501d4e41c0bf7e4cb1d
Abdallah94212 <abdallah.borji@epitech.digital>
2024-09-30 15:10:51 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+displayThisText(typeof undefined + "<br>");
+displayThisText(typeof true + "<br>");
+displayThisText(typeof 42 + "<br>");
+displayThisText(typeof 24022006n + "<br>");
+displayThisText(typeof Symbol() + "<br>");
+displayThisText(typeof {} + "<br>");
+displayThisText(typeof function(){} + "<br>");
