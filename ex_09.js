e60ef2a58a0d3dc096f6740a1c09bbfb89dea420
Abdallah94212 <abdallah.borji@epitech.digital>
2024-09-30 17:04:55 +0200

    Create ex_09.js

---
+++ ex_09.js
@@ -0,0 +1,5 @@
+displayThisText(" The variable value is "The variable value is " + " ' " + input_var + "'" 
+displayThis(" Its type is " + " ' " + typeof(input_var)"'"
+if (input_var == 42){
+displayThisText(" It is meaning of life")
+}
