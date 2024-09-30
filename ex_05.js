6216b87989784c887e886b95bd02884e3a04becf
Abdallah94212 <abdallah.borji@epitech.digital>
2024-09-30 15:36:33 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,7 @@
+var firstname = "John"
+var lastname = "Doe"
+var cardCode = "1111 2222 3333 4444"
+var experyDateMonth = 21
+var experyDateYear =  2042
+var computedBankInfo = firstname + "," + lastname + "," + cardCode + "," +  + experyDateMonth + "/" + experyDateYear
+displayInCard(computedBankInfo)
