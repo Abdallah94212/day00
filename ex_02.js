cd6c3a7ab7411f83c7cdf0300a06c12aeab7cc31
Abdallah94212 <abdallah.borji@epitech.digital>
2024-09-30 12:37:54 +0200

    Create ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,10 @@
+var bool = true
+var string = "forty two"
+var integer = 42
+var this_is_null = null 
+var float = 42.42
+document.write(bool + "<br/>")
+document.write(string + "<br/>")
+document.write(integer + "<br/>")
+document.write(this_is_null + "<br>")
+document.write(float + "<br>")
