2f4f0aa186e1160aba0c365b7cf1abf58e04688e
Abdallah94212 <abdallah.borji@epitech.digital>
2024-09-30 14:29:49 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -3,8 +3,8 @@ var string = "forty two"
 var integer = 42
 var this_is_null = null 
 var float = 42.42
-document.write(bool + "<br/>")
-document.write(string + "<br/>")
-document.write(integer + "<br/>")
-document.write(this_is_null + "<br>")
-document.write(float + "<br>")
+displayThisText(bool + "<br/>")
+displayThisText(string + "<br/>")
+displayThisText(integer + "<br/>")
+displayThisText(this_is_null + "<br>")
+displayThisText(float + "<br>")
